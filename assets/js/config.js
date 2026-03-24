15d328b80d07b069561a706093f75c6fcde2b233
wnagtz1999 <wangtiezhen1999@163.com>
2026-03-24 16:15:48 +0800

    fix: publish supabase config to pages

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,6 +2,6 @@ window.APP_CONFIG = {
   amapKey: "",
   amapSecurityJsCode: "",
   defaultCity: "全国",
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://jwjhfmtohzsddihgynoi.supabase.co",
+  supabaseAnonKey: "sb_publishable_RMVAvTB2gOLN5obbpEGhVQ_9nTqu7LD"
 };
